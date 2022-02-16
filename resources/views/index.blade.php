d56cd0f9ab5ecf35584b765760483763db1096b7
masif088 <mokhamadasif@gmail.com>
2022-02-17 00:19:57 +0700

    :)

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,7 +12,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css?_=2728291910" rel="stylesheet"
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css?_=2728291910" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('frontpage/css/normalize.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontpage/css/normalize.css?_='. time()) }}">
     <link rel="stylesheet" href="{{ asset('frontpage/css/main.css?_='. time()) }}">
 
     <style>
