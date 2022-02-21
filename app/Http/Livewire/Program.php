1d94942a4b76edfbc2a7ab263548d0890d0a4155
masif088 <mokhamadasif@gmail.com>
2022-02-21 14:37:23 +0700

    revisi 2 part 1

--- app/Http/Livewire/Program.php
+++ app/Http/Livewire/Program.php
@@ -152,17 +152,19 @@ class Program extends Component
                 $this->referralMsg = "Referral harus diisi";
             } else {
                 $r = ReferralCode::whereCode($this->referral[$id])->first();
-                if ($r->user_id==auth()->id()){
-                    $this->referralMsg = "Referral tidak ditemukan";
-                }
-                elseif ($r != null) {
-                    $b = ReferralCodeUse::whereReferralCodeId($r->id)->whereUserId(auth()->id())->get();
-                    if ($b->count()!=0) {
-                        $this->referralMsg = "Referral telah anda gunakan";
-                    } else {
-                        $this->referralMsg = "Potongan sebesar " . $this->referralBundle->referral_discount . ' ';
-                        $this->referralDiscount = $this->referralBundle->referral_discount;
-                        $this->referralUse = $r;
+                if ($r != null) {
+                    if ($r->user_id==auth()->id()){
+                        $this->referralMsg = "Referral tidak ditemukan";
+                    }
+                    else{
+                        $b = ReferralCodeUse::whereReferralCodeId($r->id)->whereUserId(auth()->id())->get();
+                        if ($b->count()!=0) {
+                            $this->referralMsg = "Referral telah anda gunakan";
+                        } else {
+                            $this->referralMsg = "Potongan sebesar " . $this->referralBundle->referral_discount . ' ';
+                            $this->referralDiscount = $this->referralBundle->referral_discount;
+                            $this->referralUse = $r;
+                        }
                     }
                 } else {
                     $this->referralMsg = "Referral tidak ditemukan";
