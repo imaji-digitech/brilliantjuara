0e81d84498ed529d166bf38f4b5b14b68c52a54f
masif088 <mokhamadasif@gmail.com>
2022-05-28 12:50:20 +0700

    revisi 2 part 10

--- resources/views/livewire/table/user-exam-ukom.blade.php
+++ resources/views/livewire/table/user-exam-ukom.blade.php
@@ -36,20 +36,27 @@
                     @endif
                 </td>
                 @php
-                $totalPoint=0;
-                $totalHigh=0;
-                foreach ($exam->examAnswers as $i => $eu) {
-                    $totalHigh+=$eu->examQuest->examStep->score_right;
-                    $answer = $eu->examQuest->answer == $eu->answer;
-                    if ($answer) {
-                        $totalPoint+=$eu->examQuest->examStep->score_right;
+                    $totalPoint=0;
+                    $totalHigh=0;
+                    foreach ($exam->examAnswers as $i => $eu) {
+                        $totalHigh+=$eu->examQuest->examStep->score_right;
+                        $answer = $eu->examQuest->answer == $eu->answer;
+                        if ($answer) {
+                            $totalPoint+=$eu->examQuest->examStep->score_right;
+                        }
                     }
-                }
                 @endphp
-                <td>{{ $exam->status==1?'-':number_format((float)($totalPoint/$totalHigh*100), 2, '.', '').'%' }}</td>
+                <td>
+                    @if($totalHigh=!0)
+                        {{ $exam->status==1?'-':number_format((float)($totalPoint/$totalHigh*100), 2, '.', '').'%' }}
+                    @else
+                        invalid try out
+                    @endif
+                </td>
+
                 <td>
                     @if (\Carbon\Carbon::now()<$exam->created_at->addMinutes($exam->exam->time) and $exam->status==1 )
-{{--                    @if($exam->status==1)--}}
+                        {{--                    @if($exam->status==1)--}}
                         <a role="button" href="{{ route('admin.user.exam.exam',[$exam->exam->slug,$exam->id]) }}"
                            class="mr-3">
                             <i class="fa fa-16px fa-forward">Start</i>
